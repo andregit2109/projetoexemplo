a5c82bb7eb93c02b832a98c2104f5cf1441ebd10
andregit2109 <usuario_git@iburger.com.br>
2026-02-21 17:30:08 -0300

    Update tres git desktop

--- ExemploGit/src/HelloDois.java
+++ ExemploGit/src/HelloDois.java
@@ -3,6 +3,5 @@ public class HelloDois {
 	static void main() {
 		System.out.println("Hello da classe dois com mais informações");
 		System.out.println("Olá André, tudo bem com você?");
-		System.out.println("Como está o curso de git?");
 	}
 }
