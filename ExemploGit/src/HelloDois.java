026b0397ae57fde33f557dd3edd1ae2c71ca57ba
andregit2109 <usuario_git@iburger.com.br>
2026-02-21 17:22:18 -0300

    Update HelloDois.java

--- ExemploGit/src/HelloDois.java
+++ ExemploGit/src/HelloDois.java
@@ -3,5 +3,6 @@ public class HelloDois {
 	static void main() {
 		System.out.println("Hello da classe dois com mais informações");
 		System.out.println("Olá André, tudo bem com você?");
+		System.out.println("Como está o curso de git?");
 	}
 }
